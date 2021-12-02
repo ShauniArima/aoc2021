7048fa9f5ed0b91c1689fd6b229b841a4e3bb2c5
Shauni <shaunisoul@protonmail.com>
2021-12-02 21:57:49 +0000

    refactor: Simplify workflow

--- day_2/Part2.hs
+++ day_2/Part2.hs
@@ -7,51 +7,37 @@ import Text.Parsec.String
 type Position       = (Integer, Integer)
 type SubmarineState = (Integer, Integer, Integer)
 
-data Command =    Forward Integer
-                    | Down Integer
-                    | Up Integer deriving (Eq)
-
 main :: IO ()
 main = do
     inputFile <- readFile "input"
-    let (Right commands) = parse parser "" inputFile
+    let (Right states) = parse parser "" inputFile
     print "Part Two:"
-    print (computeAnswer commands)
+    print (computeAnswer states)
 
-{- Compute final position -}
-computeAnswer :: [Command] -> Integer
-computeAnswer commands = uncurry (*) (computeFinalPosition commands)
+{- Compute answer -}
+computeAnswer :: [SubmarineState] -> Integer
+computeAnswer = uncurry (*) . stateToPosition . foldl computeState (0, 0, 0)
 
-computeFinalPosition :: [Command] -> Position
-computeFinalPosition commands = (stateToPosition . foldl computeState (0, 0, 0)) (commandsToStates commands)
-
-
+{- Convert state to position -}
 stateToPosition :: SubmarineState -> Position
-stateToPosition (h, d, _) = (h, d) 
+stateToPosition (h, d, _) = (h, d)
 
+{- Apply the desired operations on two states -}
 computeState :: SubmarineState -> SubmarineState -> SubmarineState
-computeState (h1, d1, aim1) (h2, d2, aim2) = (h1 + h2, d1 + (d2 * aim1), aim1 + aim2)
-
-commandsToStates :: [Command] -> [SubmarineState]
-commandsToStates = map commandToState
-
-commandToState :: Command -> SubmarineState
-commandToState (Forward value) = (value, value, 0)
-commandToState (Up value) = (0, 0, -value)
-commandToState (Down value) = (0, 0, value)
+computeState (sourceH, sourceDepth, sourceAim) (hToAdd, depthToApply, aimToAdd) = (sourceH + hToAdd, sourceDepth + (depthToApply * sourceAim), sourceAim + aimToAdd)
 
 {- Parse input -}
-parser :: Parsec String () [Command]
+parser :: Parsec String () [SubmarineState]
 parser = do
     (do directionString <- many1 letter
         space
-        getDirection directionString <$> number) `sepEndBy` newline
+        getState directionString <$> number) `sepEndBy` newline
 
 number :: Parser Integer
 number = read <$> many1 digit
 
-getDirection :: String -> Integer  -> Command
-getDirection "up" x    = Up x
-getDirection "forward" x = Forward x
-getDirection "down" x    = Down x
-getDirection s _         = error $ "Invalid input : " ++ s
+getState :: String -> Integer  -> SubmarineState
+getState "up" x    = (0, 0, -x)
+getState "down" x    = (0, 0, x)
+getState "forward" x = (x, x, 0)
+getState s _         = error $ "Invalid input : " ++ s
